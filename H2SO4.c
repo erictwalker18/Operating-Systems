cbfbc0112436510b2ef5e357c82c4c843d5f7e72
Eric Walker <erictwalker18@gmail.com>
2016-04-27 20:10:31 -0500

    All the files, close to done

---
+++ H2SO4.c
@@ -0,0 +1,245 @@
+/*
+* H2SO4 creation file
+* Authors: Eric Walker, Isaac Garfinkle
+* Last Modified: 4/26/2016
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "H2SO4.h"
+
+void delay(int);
+
+void check_sem(sem_t**, char*);
+
+
+// decalre hydrogen, sulfur, and oxygen semaphores as a global variable so
+// they are shared and accessible from all threads
+sem_t* hydro_sem;
+sem_t* oxy_sem;
+sem_t* hydro_used_sem;
+sem_t* oxy_used_sem;
+sem_t* wait_to_leave_sem;
+sem_t* sulfur_lock_sem;
+
+int main() {
+  openSems();
+
+  // create 6 threads, 4 running the Hydrogen function
+  // and 2 running the Sulfur function and
+  // 8 running the oxygen function
+  pthread_t oxy1, oxy2,oxy3, oxy4,oxy5, oxy6,oxy7, oxy8, hydro1, hydro2, hydro3, hydro4;
+  pthread_t sul1, sul2;
+  pthread_create(&hydro1, NULL, hydrogen, NULL);
+  pthread_create(&hydro2, NULL, hydrogen, NULL);
+  pthread_create(&oxy1, NULL, oxygen, NULL);
+  pthread_create(&oxy2, NULL, oxygen, NULL);
+  pthread_create(&hydro3, NULL, hydrogen, NULL);
+  pthread_create(&hydro4, NULL, hydrogen, NULL);
+  pthread_create(&sul1, NULL, sulfur, NULL);
+  pthread_create(&sul2, NULL, sulfur, NULL);
+  pthread_create(&oxy3, NULL, oxygen, NULL);
+  pthread_create(&oxy4, NULL, oxygen, NULL);
+  pthread_create(&oxy5, NULL, oxygen, NULL);
+  pthread_create(&oxy6, NULL, oxygen, NULL);
+  pthread_create(&oxy7, NULL, oxygen, NULL);
+  pthread_create(&oxy8, NULL, oxygen, NULL);
+
+  // this main program thread will wait until all 6 spawned threads exit before continuing
+  pthread_join(hydro1, NULL);
+  pthread_join(hydro2, NULL);
+  pthread_join(oxy1, NULL);
+  pthread_join(oxy2, NULL);
+  pthread_join(hydro3, NULL);
+  pthread_join(hydro4, NULL);
+  pthread_join(oxy3, NULL);
+  pthread_join(oxy4, NULL);
+  pthread_join(oxy5, NULL);
+  pthread_join(oxy6, NULL);
+  pthread_join(oxy7, NULL);
+  pthread_join(oxy8, NULL);
+  pthread_join(sul1, NULL);
+  pthread_join(sul2, NULL);
+
+  closeSems();
+  return 0;
+}
+
+
+/*
+ * Produces an H atom after a random delay, notifies sem that another H is here, exits
+ * no arguments, always returns 0
+ */
+void* hydrogen(void* args) {
+
+  // produce a hydrogen atom, takes a random amount of work with an upper bound
+  delay(rand()%5000);
+  printf("hydrogen produced\n");
+  fflush(stdout);
+
+  // post (call up) on hydrogen semaphore to signal that a hydrogen atom
+  // has been produced, then wait for it to be used in a molecule.
+  sem_post(hydro_sem);
+  sem_wait(hydro_used_sem);
+
+  printf("hydrogen exit\n");
+  fflush(stdout);
+  sem_post(wait_to_leave_sem);
+  return (void*) 0;
+}
+
+/*
+ * Produces an O atom after a random delay, checks if 2 H atoms have already been
+ * produced, if not waits for them to be produced, then creates H2O molecule and exits
+ * uses no arguments, always returns 0
+ */
+void* oxygen(void* args) {
+
+  // produce an oxygen atom, takes a random amount of work with an upper bound
+  delay(rand()%5000);
+  printf("oxygen produced\n");
+  fflush(stdout);
+
+  // post (call up) on oxygen semaphore to signal that an oxygen atom
+  // has been produced, then wait for it to be used in a molecule.
+  sem_post(oxy_sem);
+  sem_wait(oxy_used_sem);
+
+  printf("oxygen exit\n");
+  fflush(stdout);
+  sem_post(wait_to_leave_sem);
+  return (void*) 0;
+}
+
+void* sulfur(void* args) {
+  // produce a sulfur atom, takes a random amount of work with an upper bound
+  delay(rand()%5000);
+  printf("sulfur produced\n");
+  fflush(stdout);
+
+  // sulfur waits (calls down) twice on the hydrogen semaphore
+  // and four times on the oxygen semaphore
+  // meaning it cannot continue until at least 2 hydrogen atoms
+  // have been produced
+  int err = sem_wait(hydro_sem);
+  int err2 = sem_wait(hydro_sem);
+  int err3 = sem_wait(oxy_sem);
+  int err4 = sem_wait(oxy_sem);
+  int err5 = sem_wait(oxy_sem);
+  int err6 = sem_wait(oxy_sem);
+  if (err==-1 || err2==-1)
+    printf("error on sulfur wait for hydro_sem, error # %d\n", errno);
+  if  (err3==-1 || err4==-1 || err5==-1 || err6==-1)
+    printf("error on sulfur wait for oxy_sem, error # %d\n", errno);
+
+  sem_wait(sulfur_lock_sem);
+
+  // if here, know 2 hydrogen atoms have been made already so produce a water molecule
+  printf("made H2SO4\n");
+  fflush(stdout);
+
+  sem_post(hydro_used_sem);
+  sem_post(hydro_used_sem);
+
+  sem_wait(wait_to_leave_sem);
+  sem_wait(wait_to_leave_sem);
+
+
+  printf("sulfur exit\n");
+  fflush(stdout);
+
+  sem_post(oxy_used_sem);
+  sem_post(oxy_used_sem);
+  sem_post(oxy_used_sem);
+  sem_post(oxy_used_sem);
+
+  sem_wait(wait_to_leave_sem);
+  sem_wait(wait_to_leave_sem);
+  sem_wait(wait_to_leave_sem);
+  sem_wait(wait_to_leave_sem);
+
+  sem_post(sulfur_lock_sem);
+  return (void*) 0;
+
+}
+
+void openSems() {
+  // create the semaphores, very important to use last 3 arguments as shown here
+  // first argument is simply filename for semaphore, any name is fine but must be a valid path
+  hydro_sem = sem_open("hydrosmphr", O_CREAT|O_EXCL, 0466, 0);
+  oxy_sem = sem_open("oxysmphr", O_CREAT|O_EXCL, 0466, 0);
+  hydro_used_sem = sem_open("hydroused", O_CREAT|O_EXCL, 0466, 0);
+  oxy_used_sem = sem_open("oxyused", O_CREAT|O_EXCL, 0466, 0);
+  wait_to_leave_sem = sem_open("leave", O_CREAT|O_EXCL, 0466, 0);
+  sulfur_lock_sem = sem_open("sulflock", O_CREAT|O_EXCL, 0466, 0);
+
+
+  check_sem(&hydro_sem, "hydrosmphr");
+  check_sem(&oxy_sem, "oxysmphr");
+  check_sem(&hydro_used_sem, "hydroused");
+  check_sem(&oxy_used_sem, "oxyused");
+  check_sem(&wait_to_leave_sem, "leave");
+  check_sem(&sulfur_lock_sem, "sulflock");
+
+  sem_post(sulfur_lock_sem);
+}
+void closeSems() {
+  // important to BOTH close the semaphore object AND unlink the semaphore file
+  // for each semaphore
+  sem_close(hydro_sem);
+  sem_unlink("hydrosmphr");
+  sem_close(oxy_sem);
+  sem_unlink("oxysmphr");
+  sem_close(hydro_used_sem);
+  sem_unlink("hydroused");
+  sem_close(oxy_used_sem);
+  sem_unlink("oxyused");
+  sem_close(wait_to_leave_sem);
+  sem_unlink("leave");
+  sem_close(sulfur_lock_sem);
+  sem_unlink("sulflock");
+}
+
+void check_sem(sem_t** semaphore, char* sem_string) {
+  // *** opening semaphores using C on a unix system creates an actual semaphore file that is not
+  // automatically closed or deleted when the program exits.  As long as you close the semaphore AND
+  // unlink the filename you gave in sem_open, you won't have any problems, but if you forget, or if
+  // your program crashes in the middle or you have to quit using ctrl-c or something similar, you
+  // will get an error when you try to run your program again because the semaphore file will already
+  // exist. ***
+  // The following code handles the above issue by deleting the sempahore file if it already existed
+  // and then creating a new one.  It also handles issues where you are not allowed to create/open a
+  // new file, e.g. you do not have permission at the given location.
+  while (*semaphore==SEM_FAILED) {
+    if (errno == EEXIST) {
+      printf("semaphore %s already exists, unlinking and reopening\n", sem_string);
+      fflush(stdout);
+      sem_unlink(sem_string);
+      *semaphore = sem_open(sem_string, O_CREAT|O_EXCL, 0466, 0);
+    }
+    else {
+      printf("semaphore could not be opened, error # %d\n", errno);
+      fflush(stdout);
+      exit(1);
+    }
+  }
+}
+
+/*
+ * NOP function to simply use up CPU time
+ * arg limit is number of times to run each loop, so runs limit^2 total loops
+ */
+void delay( int limit )
+{
+  int j, k;
+
+  for( j=0; j < limit; j++ )
+    {
+      for( k=0; k < limit; k++ )
+        {
+        }
+    }
+}
---
+++ boat.c
@@ -0,0 +1,362 @@
+/*
+* H2SO4 creation file
+* Authors: Eric Walker, Isaac Garfinkle
+* Last Modified: 4/26/2016
+*/
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+// Locks for each condition variable
+pthread_mutex_t boat_mutex;
+pthread_mutex_t children_start_mutex;
+pthread_mutex_t children_end_mutex;
+pthread_mutex_t adult_mutex;
+pthread_mutex_t done_mutex;
+// Condition variables
+pthread_cond_t boat;
+pthread_cond_t start;
+pthread_cond_t end;
+pthread_cond_t adult_cond;
+// Semaphores for initializing and exiting
+sem_t* threads_initialized;
+sem_t* threads_at_end;
+
+// Shared variables for the threads (not to be used in main)
+int adults_start=0;
+int children_start=0;
+int children_end=0;
+int boat_pass=0;
+int done=0;
+
+void* adult(void*);
+void* child(void*);
+void initSynch();
+void closeSynch();
+// local function just used to randomly shuffle creation order of atoms
+void shuffle(int*, int);
+// local function used to check that semaphores are available
+void check_sem(sem_t**, char*);
+
+
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    printf("Need to put in adult and children numbers\n");
+    exit(0);
+  }
+  // set # to create of each atom (atoi converts a string to an int)
+  const int num_adults = atoi(argv[1]);
+  const int num_children = atoi(argv[2]);
+  const int total = num_adults+num_children;
+
+  initSynch();
+
+  //--------------Create threads -----------------
+  // add desired number of each type of person
+  int order[total];
+  int i;
+  for (i=0; i<num_adults; i++) {
+    order[i] = 1;
+  }
+  for (; i<num_adults+num_children; i++) {
+    order[i] = 2;
+  }
+
+  // order now has # of 1's and 2's to reflect # of 2 types of people,
+  // so just need to shuffle to get random order
+  shuffle(order, total);
+
+  // now create threads in shuffled order
+  pthread_t people[total];
+  long adult_counter=0;
+  long child_counter=0;
+  for (i=0; i<total; i++) {
+    if (order[i]==1) {
+      adult_counter++;
+      pthread_create(&people[i], NULL, adult, (void *)adult_counter);
+    }
+    else if (order[i]==2) {
+      child_counter++;
+      pthread_create(&people[i], NULL, child, (void*)child_counter);
+    }
+    else printf("Something went horribly wrong!!!\n");
+  }
+  //--------------End create threads -----------------
+
+  // Wait for all the threads to be created, then make the
+  // boat show up
+  for (i=0; i<total; i++) {
+    sem_wait(threads_initialized);
+  }
+
+  // Signal a child to start
+  pthread_cond_signal(&start);
+
+  // Wait for everyone to get across, then exit
+  sem_wait(threads_at_end);
+
+  exit(0);
+
+}
+
+
+// ---------------------------Adult and Child threads---------------------------
+void* adult(void* args) {
+  long my_id = (long)args;
+  // tell others you're ready
+  printf("Adult %ld arrived on Oahu.\n", my_id);
+  fflush(stdout);
+  sem_post(threads_initialized);
+
+  // Get in the adult queue
+  pthread_mutex_lock(&adult_mutex);
+  adults_start++;
+  pthread_cond_wait(&adult_cond, &adult_mutex);
+  pthread_mutex_unlock(&adult_mutex);
+
+  // once out of the adult queue, there must be 1 or 0 children
+  // on the start island, so row over, signal a child to row back
+  // unless there is no one left on the start island
+
+  printf("Adult %ld getting in boat on Oahu.\n", my_id);
+  fflush(stdout);
+  //Once the adult gets on the boat, it will decrease the adults on the island
+  pthread_mutex_lock(&adult_mutex);
+  adults_start--;
+  pthread_mutex_unlock(&adult_mutex);
+
+  printf("Adult %ld rowing boat from Oahu to Molokai.\n", my_id);
+  fflush(stdout);
+
+  printf("Adult %ld leaving boat on Molokai.\n", my_id);
+  fflush(stdout);
+  // Once adult is on the end (molokai) island, it will signal a child
+  // to row back to the start (oahu) island, then exit
+  pthread_cond_signal(&end);
+
+  pthread_exit(NULL);
+}
+
+// Child thread
+// Loops through getting on boat until everyone is on the last island
+void* child(void* args) {
+  long my_id = (long)args;
+
+  pthread_mutex_lock(&children_start_mutex);
+  children_start++;
+  // initialize where you are, and tell main you're ready
+  printf("Child %ld arrive on start (Oahu)\n", my_id);
+  fflush(stdout);
+  sem_post(threads_initialized);
+  // get in the starting queue, but let other children start
+  pthread_cond_wait(&start, &children_start_mutex);
+  pthread_mutex_unlock(&children_start_mutex);
+
+  while(1==1) {
+    // If there are exactly 2 children and 0 adults, we'll be done after
+    // This boat trip, so save that.
+    pthread_mutex_lock(&children_start_mutex);
+    pthread_mutex_lock(&adult_mutex);
+    pthread_mutex_lock(&done_mutex);
+    if (adults_start==0 && children_start == 2) {
+      done=1;
+    }
+    pthread_mutex_unlock(&done_mutex);
+    pthread_mutex_unlock(&adult_mutex);
+    pthread_mutex_unlock(&children_start_mutex);
+
+
+    pthread_mutex_lock(&boat_mutex);
+    // Once child has been signaled that there's a spot to go to
+    // the child should get on the boat, go in pairs
+    printf("Child %ld getting in boat on Oahu.\n", my_id);
+    fflush(stdout);
+    boat_pass++;
+    //Signal another child to get in the boat.
+    if (boat_pass<2) {
+      pthread_cond_signal(&start);
+    }
+    // Wait until another child gets in the boat.
+    while (boat_pass<2) {
+      pthread_cond_wait(&boat, &boat_mutex);
+    }
+    // If another child is already in the boat, signal them to wake up.
+    pthread_cond_signal(&boat);
+
+    //row the boat
+    printf("Child %ld rowing boat from Oahu to Molokai.\n", my_id);
+    fflush(stdout);
+    pthread_mutex_lock(&children_start_mutex);
+    children_start--;
+    pthread_mutex_unlock(&children_start_mutex);
+
+
+
+    // get out of the boat
+    printf("Child %ld leaving boat on Molokai.\n", my_id);
+    fflush(stdout);
+
+    // Leave the boat and get on the end island, but wait for other child
+    // to row to end
+    if (boat_pass>1) {
+      pthread_cond_wait(&boat, &boat_mutex);
+    }
+    boat_pass--;
+    pthread_cond_signal(&boat);
+    pthread_mutex_unlock(&boat_mutex);
+
+    pthread_mutex_lock(&children_end_mutex);
+    children_end++;
+
+
+    // If we decided we're done, then tell main we're done, and
+    // let main run.
+    pthread_mutex_lock(&done_mutex);
+    if (done == 1) {
+      sem_post(threads_at_end);
+      pthread_cond_wait(&boat, &done_mutex);
+    }
+
+    // signal the end queue to send one back, then wait in the queue
+    pthread_cond_signal(&end);
+    pthread_mutex_unlock(&done_mutex);
+
+    pthread_cond_wait(&end, &children_end_mutex);
+    // release the lock
+    pthread_mutex_unlock(&children_end_mutex);
+
+
+
+
+
+    // go back to first island
+    // ...................
+    pthread_mutex_lock(&boat_mutex);
+    // Once child has been signaled that there's a spot to go to
+    // the child should get on the boat, then go back alone.
+    printf("Child %ld getting in boat on Molokai.\n", my_id);
+    fflush(stdout);
+    boat_pass++;
+    pthread_mutex_unlock(&boat_mutex);
+
+    //row the boat
+    printf("Child %ld rowing boat from Molokai to Oahu.\n", my_id);
+    fflush(stdout);
+
+    pthread_mutex_lock(&children_start_mutex);
+    children_end--;
+    pthread_mutex_unlock(&children_start_mutex);
+
+    // get out of the boat
+    printf("Child %ld leaving boat on Oahu.\n", my_id);
+    fflush(stdout);
+
+    // Leave the boat and get on the start island.
+    pthread_mutex_lock(&boat_mutex);
+    boat_pass--;
+    pthread_mutex_unlock(&boat_mutex);
+
+    // Leave the boat and get on the start island, signal children
+    // that there is a boat here to get into, if no children besides
+    // self, then signal an adult to go.
+    pthread_mutex_lock(&children_start_mutex);
+    children_start++;
+    if (children_start>1) {
+      pthread_cond_signal(&start);
+    } else {
+      pthread_cond_signal(&adult_cond);
+    }
+    // Get in the start queue
+    pthread_cond_wait(&start, &children_start_mutex);
+    pthread_mutex_unlock(&children_start_mutex);
+
+  }
+
+  pthread_exit(NULL);
+}
+
+// -------------------------End Adult and Child threads-------------------------
+
+void initSynch() {
+  //Create all the mutexes and condition variables
+  pthread_mutex_init(&boat_mutex, NULL);
+  pthread_mutex_init(&children_start_mutex, NULL);
+  pthread_mutex_init(&children_end_mutex, NULL);
+  pthread_mutex_init(&adult_mutex, NULL);
+  pthread_mutex_init(&done_mutex, NULL);
+  pthread_cond_init(&boat, NULL);
+  pthread_cond_init(&start, NULL);
+  pthread_cond_init(&end, NULL);
+  pthread_cond_init(&adult_cond, NULL);
+
+  // Create start/end semaphores
+  threads_initialized = sem_open("startsmphr", O_CREAT|O_EXCL, 0466, 0);
+  threads_at_end = sem_open("endsmphr", O_CREAT|O_EXCL, 0466, 0);
+
+  check_sem(&threads_initialized, "startsmphr");
+  check_sem(&threads_at_end, "endsmphr");
+}
+
+void closeSynch() {
+  // Close the mutexes, condition variables and semaphores
+  // Close the threads and mutexes
+  /*
+  pthread_mutex_destroy(&boat_mutex);
+  pthread_mutex_destroy(&children_start_mutex);
+  pthread_mutex_destroy(&children_end_mutex);
+  pthread_mutex_destroy(&adult_mutex);
+  pthread_mutex_destroy(&done_mutex);
+  pthread_cond_destroy(&boat);
+  pthread_cond_destroy(&start);
+  pthread_cond_destroy(&end);
+  pthread_cond_destroy(&adult_cond);
+  */
+  // important to BOTH close the semaphore object AND unlink the semaphore file
+  // for each semaphore
+  sem_close(threads_initialized);
+  sem_unlink("startsmphr");
+  sem_close(threads_at_end);
+  sem_unlink("endsmphr");
+
+  pthread_exit (NULL);
+}
+
+void shuffle(int* intArray, int arrayLen) {
+  int i=0;
+  for (i=0; i<arrayLen; i++) {
+    int r = rand()%arrayLen;
+    int temp = intArray[i];
+    intArray[i] = intArray[r];
+    intArray[r] = temp;
+  }
+}
+
+void check_sem(sem_t** semaphore, char* sem_string) {
+  // *** opening semaphores using C on a unix system creates an actual semaphore file that is not
+  // automatically closed or deleted when the program exits.  As long as you close the semaphore AND
+  // unlink the filename you gave in sem_open, you won't have any problems, but if you forget, or if
+  // your program crashes in the middle or you have to quit using ctrl-c or something similar, you
+  // will get an error when you try to run your program again because the semaphore file will already
+  // exist. ***
+  // The following code handles the above issue by deleting the sempahore file if it already existed
+  // and then creating a new one.  It also handles issues where you are not allowed to create/open a
+  // new file, e.g. you do not have permission at the given location.
+  while (*semaphore==SEM_FAILED) {
+    if (errno == EEXIST) {
+      //printf("semaphore %s already exists, unlinking and reopening\n", sem_string);
+      //fflush(stdout);
+      sem_unlink(sem_string);
+      *semaphore = sem_open(sem_string, O_CREAT|O_EXCL, 0466, 0);
+    }
+    else {
+      printf("semaphore could not be opened, error # %d\n", errno);
+      fflush(stdout);
+      exit(1);
+    }
+  }
+}
